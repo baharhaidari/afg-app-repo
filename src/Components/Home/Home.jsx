9766d2632b8504c80b0df542b4c7ebafabb5b657
BaharHaidary <117345907+BaharHaidary@users.noreply.github.com>
2024-05-17 17:38:41 +0430

    Home section created

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,19 @@
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <section className="min-h-screen flex justify-center items-center flex-col px-40 gap-8">
+      <h1 className="text-8xl">Time to Explore Afghanistan</h1>
+      <p className="text-2xl text-center px-32">
+        Immerse yourself in the rich cultural heritage and breathtaking
+        landscapes of Afghanistan, a land where ancient history and natural
+        beauty converge. Discover the bustling bazaars of Kabul, the serene
+        gardens of Herat, and the majestic Buddha niches of Bamiyan, each
+        offering a unique glimpse into the country's storied past and vibrant
+        present. Visit our site to explore these popular destinations and
+        uncover the hidden gems that make Afghanistan a truly captivating and
+        unforgettable experience.
+      </p>
+    </section>
+  );
+}

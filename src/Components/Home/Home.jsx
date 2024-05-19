4ea540ae921ffe395cb7b56fb13603dc77d07473
BaharHaidary <117345907+BaharHaidary@users.noreply.github.com>
2024-05-19 18:24:45 +0430

    Home and Navbar created

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,9 +2,9 @@ import "./Home.css";
 
 export default function Home() {
   return (
-    <section className="min-h-screen flex justify-center items-center flex-col px-40 gap-8">
-      <h1 className="text-8xl">Time to Explore Afghanistan</h1>
-      <p className="text-2xl text-center px-32">
+    <section className="min-h-screen flex justify-center items-center flex-col px-28 gap-8  home__section ">
+      <h1 className="text-8xl home__title">Time to Explore Afghanistan</h1>
+      <p className="text-center px-32 home__paragraph">
         Immerse yourself in the rich cultural heritage and breathtaking
         landscapes of Afghanistan, a land where ancient history and natural
         beauty converge. Discover the bustling bazaars of Kabul, the serene
@@ -14,6 +14,7 @@ export default function Home() {
         uncover the hidden gems that make Afghanistan a truly captivating and
         unforgettable experience.
       </p>
+      <button>EXPLORE NOW</button>
     </section>
   );
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,18 +2,18 @@ import "./Navbar.css";
 
 export default function Navbar() {
   return (
-    <nav className="w-full flex justify-between px-14 py-6 items-center navbar">
+    <nav className="w-full flex justify-between px-14 py-6 items-center navbar sticky top-0 ">
       <div>
         <a href="" className="text-4xl font-medium">
           Logo
         </a>
       </div>
 
-      <ul className="flex justify-between gap-8 items-center">
-        <li className="text-xl">Home</li>
-        <li className="text-xl">About</li>
-        <li className="text-xl">Popular Places</li>
-        <li className="text-xl">Contact</li>
+      <ul className="flex justify-between gap-8 items-center navlinks__container">
+        <li className="text-xl p-2">Home</li>
+        <li className="text-xl p-2">About</li>
+        <li className="text-xl p-2">Popular Places</li>
+        <li className="text-xl p-2">Contact</li>
       </ul>
 
       <div className="flex gap-8 text-center items-center">
